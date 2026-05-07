f8556ad02de4dc7985a958e625513664c950e1fd
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-05-07 16:06:11 -0500

    I added a function that cleans that data and take out any duplicates. In this dataset there was 56 dupicaltes in the review column. This is refelcted in the count when compareing both datasets

--- data/raw.py
+++ data/raw.py
@@ -17,18 +17,38 @@ import pandas as pd
       'reviews-6805-9035.csv', 
       'reviews-9035-11265.csv']"""
 
-# Load the dataset (adjust the file name as needed)
-
+# Load the dataset and is able to input the number of rows you want.
 def load_data(nrows):
     path = kagglehub.dataset_download("forgemaster/steam-reviews-dataset")
     file_path = os.path.join(path, "reviews-11265-13495.csv")
     df = pd.read_csv(file_path, encoding="latin-1", engine="python", on_bad_lines="skip", nrows=nrows)
     return df
 
+# Cleans that data by removing any rows with missing values and duplicates, and also converts the 'review' column to lowercase for consistency.
+
+def clean_data(df):
+    df = df.copy()
+    df = df.drop_duplicates()
+    df = df.dropna(subset=["review"])
+    df["review"] = df["review"].str.lower()
+
+    
+    return df
+
+      
+
+
+
 
 raw_data = load_data(50000)
 
-print(raw_data.head())
+clean_df = clean_data(raw_data)
+
+
+print(raw_data.shape)
+
+print(clean_df.shape)
+
 
 
 
